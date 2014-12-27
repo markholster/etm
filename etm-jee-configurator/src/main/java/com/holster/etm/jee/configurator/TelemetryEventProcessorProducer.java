9ab19bec84ab1b1aeb55966c240b53a71d2d1d32
Mark Holster <m.holster@home.nl>
2014-12-27 12:10:51 +0000

    Custom threadpool to add names to threads.

--- etm-jee-configurator/src/main/java/com/holster/etm/jee/configurator/TelemetryEventProcessorProducer.java
+++ etm-jee-configurator/src/main/java/com/holster/etm/jee/configurator/TelemetryEventProcessorProducer.java
@@ -2,6 +2,8 @@ package com.holster.etm.jee.configurator;
 
 import java.util.Properties;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PreDestroy;
@@ -21,34 +23,59 @@ public class TelemetryEventProcessorProducer {
 	@EtmConfiguration
 	@Inject
 	private Properties configration;
-	
+
 	@Inject
 	private Session session;
-	
+
 	@Inject
 	private SolrServer solrServer;
-	
+
 	private TelemetryEventProcessor telemetryEventProcessor;
 
 	@Produces
 	public TelemetryEventProcessor getTelemetryEventProcessor() {
 		synchronized (this) {
-	        if (this.telemetryEventProcessor == null) {
-	        	this.telemetryEventProcessor = new TelemetryEventProcessor();
-	        	int enhancingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.enhancing_handler_count", "5"));
-	        	int indexingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.indexing_handler_count", "5"));
-	        	int persistingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.persisting_handler_count", "5"));
-	        	int ringbufferSize = Integer.valueOf(this.configration.getProperty("etm.ringbuffer_size", "4096"));
-	        	this.telemetryEventProcessor.start(Executors.newCachedThreadPool(), this.session, this.solrServer, ringbufferSize, enhancingHandlerCount, indexingHandlerCount, persistingHandlerCount);
-	        }
-        }
+			if (this.telemetryEventProcessor == null) {
+				this.telemetryEventProcessor = new TelemetryEventProcessor();
+				int enhancingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.enhancing_handler_count", "5"));
+				int indexingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.indexing_handler_count", "5"));
+				int persistingHandlerCount = Integer.valueOf(this.configration.getProperty("etm.persisting_handler_count", "5"));
+				int ringbufferSize = Integer.valueOf(this.configration.getProperty("etm.ringbuffer_size", "4096"));
+				this.telemetryEventProcessor.start(Executors.newCachedThreadPool(new EtmThreadFactory()), this.session, this.solrServer,
+				        ringbufferSize, enhancingHandlerCount, indexingHandlerCount, persistingHandlerCount);
+			}
+		}
 		return this.telemetryEventProcessor;
 	}
-	
+
 	@PreDestroy
 	public void preDestroy() {
 		if (this.telemetryEventProcessor != null) {
 			this.telemetryEventProcessor.stopAll();
+		}
+	}
+
+	private static class EtmThreadFactory implements ThreadFactory {
+		private static final AtomicInteger poolNumber = new AtomicInteger(1);
+		private final ThreadGroup group;
+		private final AtomicInteger threadNumber = new AtomicInteger(1);
+		private final String namePrefix;
+
+		EtmThreadFactory() {
+			SecurityManager securityManager = System.getSecurityManager();
+			this.group = (securityManager != null) ? securityManager.getThreadGroup() : Thread.currentThread().getThreadGroup();
+			this.namePrefix = "etmpool-" + poolNumber.getAndIncrement() + "-thread-";
+		}
+
+		public Thread newThread(Runnable runnable) {
+			Thread thread = new Thread(this.group, runnable, this.namePrefix + this.threadNumber.getAndIncrement(), 0);
+			if (thread.isDaemon()) {
+				thread.setDaemon(false);
+			}
+			if (thread.getPriority() != Thread.NORM_PRIORITY) {
+				thread.setPriority(Thread.NORM_PRIORITY);
+			}
+			return thread;
 		}
 	}
 }
