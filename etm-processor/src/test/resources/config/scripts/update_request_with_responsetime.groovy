5da3c0395d4770ca8f81c2d3912451549627e07f
Mark Holster <m.holster@home.nl>
2015-09-19 11:10:56 +0200

    Error in updatescript. The responsetime came out negative.

--- etm-processor/src/test/resources/config/scripts/update_request_with_responsetime.groovy
+++ etm-processor/src/test/resources/config/scripts/update_request_with_responsetime.groovy
@@ -1,6 +1,6 @@
 if (ctx._source.writing_endpoint_handler.handling_time) {
-	if (ctx._source.response_time && ctx._source.response_time > (ctx._source.writing_endpoint_handler.handling_time - response_handling_time)) {
-		ctx._source.response_time = ctx._source.writing_endpoint_handler.handling_time - response_handling_time;
+	if (ctx._source.response_time && ctx._source.response_time > (response_handling_time - ctx._source.writing_endpoint_handler.handling_time)) {
+		ctx._source.response_time = response_handling_time - ctx._source.writing_endpoint_handler.handling_time;
 	}
 } else {
 	if (!ctx._source.response_handling_time || (ctx._source.response_handling_time > response_handling_time)) {
