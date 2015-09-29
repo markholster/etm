f5dc7967513620b2a2712d996a22651adcd5d548
Mark Holster <m.holster@home.nl>
2015-09-29 21:33:16 +0200

    Created a reminded for the ElasticBackedEtmConfigurationTest.

---
+++ etm-processor/src/test/java/com/jecstar/etm/processor/elastic/ElasticBackedEtmConfigurationTest.java
@@ -0,0 +1,18 @@
+package com.jecstar.etm.processor.elastic;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Test class for the <code>ElasticBackedEtmConfiguration</code> class.
+ * 
+ * @author mark
+ */
+public class ElasticBackedEtmConfigurationTest {
+
+	@Test
+	public void testAllConfigurationKeysDefined() {
+		fail("Not implemented");
+	}
+}
