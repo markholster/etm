b94cfff8739514a7dad323e84b0a2742712cc1ca
Mark Holster <m.holster@home.nl>
2015-11-07 10:14:34 +0100

    Added testcase in processor.

---
+++ etm-processor/src/test/java/com/jecstar/etm/processor/elastic/ElasticBackedEtmConfigurationTest.java
@@ -0,0 +1,35 @@
+package com.jecstar.etm.processor.elastic;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jecstar.etm.core.configuration.EtmConfiguration;
+
+/**
+ * Test class for the <code>ElasticBackedEtmConfiguration</code> class.
+ * 
+ * @author mark
+ */
+public class ElasticBackedEtmConfigurationTest {
+
+	/**
+	 * Test if the <code>ElasticBackedEtmConfiguration</code> overrides all
+	 * getter methods of the <code>EtmConfiguration</code> class.
+	 */
+	@Test
+	public void testAllConfigurationKeysDefined() {
+		Method[] declaredMethods = EtmConfiguration.class.getDeclaredMethods();
+		Method[] overriddenMethods = ElasticBackedEtmConfiguration.class.getDeclaredMethods();
+		for (Method method : declaredMethods) {
+			String methodName = method.getName(); 
+			if (methodName.startsWith("get") && !methodName.equals("getNodeName")
+					&& !methodName.equals("getComponent")) {
+				Assert.assertTrue("Methode '" + methodName + "' not overriden in " + ElasticBackedEtmConfiguration.class.getName(),
+						Arrays.stream(overriddenMethods).anyMatch(p -> p.getName().equals(methodName)));
+			}
+		}
+	}
+}
