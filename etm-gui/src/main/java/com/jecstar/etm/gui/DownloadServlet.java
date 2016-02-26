2f4018bd415ba395b64e5098c20fb51c74461bbc
Mark Holster <m.holster@home.nl>
2016-02-26 10:57:32 +0100

    Added a download servlet.

---
+++ etm-gui/src/main/java/com/jecstar/etm/gui/DownloadServlet.java
@@ -0,0 +1,109 @@
+package com.jecstar.etm.gui;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHitField;
+import org.elasticsearch.search.sort.SortOrder;
+
+import com.jecstar.etm.core.converter.TelemetryEventConverterTags;
+import com.jecstar.etm.core.converter.json.TelemetryEventConverterTagsJsonImpl;
+import com.jecstar.etm.jee.configurator.core.GuiConfiguration;
+
+@WebServlet("/search/download")
+public class DownloadServlet extends HttpServlet {
+
+	@GuiConfiguration
+	@Inject
+	private Client elasticClient;
+	
+	private static final long serialVersionUID = -5943929079966118935L;
+	
+	private static final DateFormat csvDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	
+	private final TelemetryEventConverterTags tags = new TelemetryEventConverterTagsJsonImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String queryString = req.getParameter("queryString");
+		String start = req.getParameter("start");
+		String rows = req.getParameter("rows");
+		String sortField = req.getParameter("sortField");
+		String sortOrder = req.getParameter("sortOrder");
+		
+		int startIx = 0;
+		int nrOfRows = 500;
+		if (start != null && start.trim().length() > 0) {
+			startIx = Integer.valueOf(start);
+		}
+		if (rows != null && rows.trim().length() > 0) {
+			nrOfRows = Integer.valueOf(start);
+			if (nrOfRows > 500) {
+				nrOfRows = 500;
+			}
+		}
+		SearchRequestBuilder searchRequest = this.elasticClient.prepareSearch("etm_event_all")
+				.setFrom(startIx)
+				.setSize(nrOfRows)
+				.setQuery(QueryBuilders.queryStringQuery(queryString)
+						.defaultField("content")
+						.lowercaseExpandedTerms(false)
+						.analyzeWildcard(true)
+// TODO Hard coded for Achmea, we need this to be an input field from the browser/user.						
+						.timeZone("Europe/Amsterdam"));
+		if (sortField != null && sortField.trim().length() > 0) {
+			if ("asc".equalsIgnoreCase(sortOrder)) {
+				searchRequest.addSort(sortField, SortOrder.ASC);
+			} else {
+				searchRequest.addSort(sortField, SortOrder.DESC);
+			}
+		}
+		SearchResponse searchResponse = searchRequest.get();
+		resp.setContentType("text/csv");
+		resp.getWriter().write("\"id\",\"correlation id\",\"creation time\",\"content\"\r\n");
+		for (SearchHit hit : searchResponse.getHits()) {
+			Map<String, Object> valueMap = hit.getSource();
+			resp.getWriter().write("\"" + hit.getId()+ "\",\"" + getStringValue(this.tags.getCorrelationIdTag(), valueMap) + "\",\"" + getDateValue(this.tags.getCreationTimeTag(), valueMap) + "\",\"" + getStringValue(this.tags.getContentTag(), valueMap)+ "\"\r\n");
+		}
+		resp.flushBuffer();
+	}
+	
+	private String getDateValue(String key, Map<String, ?> valueMap) {
+		if (valueMap.containsKey(key)) {
+			Object object = valueMap.get(key);
+			if (object instanceof SearchHitField) {
+				long time = ((SearchHitField)object).getValue(); 
+				return csvDateFormat.format(new Date(time)).replaceAll("\"", "\"\"");
+			}
+			return csvDateFormat.format(new Date(((Number)valueMap.get(key)).longValue())).replaceAll("\"", "\"\"");
+		}
+		return "";
+	}
+	
+	private String getStringValue(String key, Map<String, ?> valueMap) {
+		if (valueMap.containsKey(key)) {
+			Object object = valueMap.get(key);
+			if (object instanceof SearchHitField) {
+				return ((SearchHitField)object).getValue().toString().replaceAll("\"", "\"\"");
+			}
+			return valueMap.get(key).toString().replaceAll("\"", "\"\"");
+		}
+		return "";
+	}
+
+}
