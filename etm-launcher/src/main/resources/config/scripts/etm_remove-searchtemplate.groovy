c289e7ca4c1d7a6c716e09d73d152720449be36f
Mark Holster <m.holster@home.nl>
2016-04-01 23:09:23 +0200

    Bug in groovy script since new version of groovy.

--- etm-launcher/src/main/resources/config/scripts/etm_remove-searchtemplate.groovy
+++ etm-launcher/src/main/resources/config/scripts/etm_remove-searchtemplate.groovy
@@ -1,7 +1,7 @@
 if (name) {
     if (ctx._source.searchtemplates) {
         ctx._source.searchtemplates.removeAll { item -> item.name.equals(name)};
-        if (ctx._source.searchtemplates.size == 0) {
+        if (ctx._source.searchtemplates.isEmpty() == 0) {
             ctx._source.searchtemplates = null;
         }
     } else {
