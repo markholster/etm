b89c069fa5613e4e3205f55b4ac725fe071baad2
Mark Holster <m.holster@home.nl>
2016-04-04 20:52:55 +0200

    License for Achmea till april 15th 2017.

--- etm-license-generator/src/main/java/com/jecstar/etm/license/LicenseGenerator.java
+++ etm-license-generator/src/main/java/com/jecstar/etm/license/LicenseGenerator.java
@@ -21,7 +21,7 @@ public class LicenseGenerator {
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-		calendar.set(Calendar.YEAR, 2016);
+		calendar.set(Calendar.YEAR, 2017);
 		calendar.set(Calendar.MONTH, Calendar.APRIL);
 		calendar.set(Calendar.DAY_OF_MONTH, 15);
 		calendar.set(Calendar.HOUR_OF_DAY, 23);
--- etm-license-generator/src/main/java/com/jecstar/etm/license/LicenseReader.java
+++ etm-license-generator/src/main/java/com/jecstar/etm/license/LicenseReader.java
@@ -19,7 +19,7 @@ public class LicenseReader {
 	private static final String PUBLIC_KEY = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAtBCraSZNjqfqnDK/ESEqwZWZiDY6YRe72N8id//B5LHv7eO41cgRrKzAIn+WH10C3jOjGpJjF1RITKTJg1FM4CK+L66hYP3HQVX8ghtQT99TkHuTkTGxbbBMZd4VF77TR5mTa0LjMTGz7+r9q0PAQEGPol/WqaOTxGHiizh7/qmA0hvAA4Ff39T0CsFyWFpI4hmfS5JG/sLsG8WKd125A1VJFk76ZH7kWP1ysrzGzbR1vSQznQpzz7GPpbzFgjDWJpvQzLREv7qSn1z7MGD4YKlLpgaYxoPUsF2kg4N3YzvZw+RfMTFS2v689VmLccZbySXSoqXyssSq6oMlXIwDSus5qFaB1TeYFJWHZh/t6QHHYeyI0RW6pzIAAG/yGF9uX13uiIb9J9+Qu02XAPstl0ZsVfAVdbzV1AKFMPVOCzMHk6T8YcLsFKedigeH4K2vzdQyHC4L0oZ+2xYiDp904Y7A20HfTyBhVJmz7OIKLjJbnuCh8wP1g9VAR9NC468/nhEdCBxT2nHvvJMLzw2xUBYNIoSw5rWd5+nO9kiCWD7OoNpL5nTRRlX3jBpuqEJmszQo3wF0jZEqAi/pYn3c60iEljtx8m8K8EgjylS/C49qBDUfCQnwfNQxGjxzEzeFc9+mJRox87kxYMUsCyT5u46f8P1wfHOWxzubRgcr0hECAwEAAQ==";
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		final String licenseKey = "MSx50HWEoub86CtuBzpvrNyG1DyO00dp+k1TBqcmHzLOKYcvXoRgwlDhTSe/ABOwUgygrHHZow+hyyqa3Y0y7PoUSeGWce6/l0ljtA5ZdpR2M/ff54yej3Kgy6XsJ3JSIIW/3AlSm36nMDvbPH1bhxtcq7R7fPzo9NBLn3MoPq7ArZrVbpUkGEMh1iVqvdgb7OqKdwYK2a70Ku12BHHz8mPybRE73a5bqtc2UadH+H2nn/RYvq1zQ1jiuIbGTmm214dpIltSo569PFS5FYggWUqoJfMrnpVCk/KeQRKt8xx5fwGn3C/iaCGIUD53MY1gn4UPdG/GUXMaz9Zm3GKRwbzy0MvUD1jmd9c4iR68duJelmvYIiIotwLj81K/+s1oG7rh40azVbLs+1IC687eO2BSyEVRMEewIGzVN98nHAzYrek25GBhlbv6AN7QnzfszJkrKmSIwxAHj8UxmisdAGyMr/84t4PDOMQLzgteMWoLWK2smVeCvSbj9YG7IOil0q2dNJMzyWFsusm1NCjMHpHzwOdy4ouowvk9sxOzeYjQu6QpqXxEKslg1XrjC+Vehj1b5ukAh/ZmH+L9iSRchkFCBiUJDQUlbBPRYLtWo/VyQqYJu1ricUrMKftAtKgJ31QbuXWSKaEMDLMJ/E+f/uE9lNKmtlT3CYOKavwNOUI=";
+		final String licenseKey = "Fh/7W/Y77Y6xqO6aBWY4ouC3DljJk0rA7/DYMgB6BOtXZ8uRVyovWzdfB9gSiSpIJdPkuNxLKTH9j90fPsgMV8gkG2kX8u56zLN2u7hYBXAhX0+s3TEEobCei+MlB+nzLYtgomu62RdmL18uoFL27JR9rV8vFM3bN1L3YigmtytkKJ4Ton9nugqsBW6Me55qHSsGXqQw0D+veCoOG9xijMyQ+1pDwycFBTMGsFsgdLGzXy/qtBK4caAm7BfXkj5iutD6oD9Ynt2Y72RBqsdC0prmOk6V05OngTY/7onKQhiQ186f8zjl5/d37eT8DnT9AQWjOgPTuJVZhkzlmk9KLdBxOccbUKkuAplRFC4IFcs+mYeF6sBX2R2nkyI6hdtdRJNpZ2p6jxbfh9t+cflKz3I+FdrLqHJYt9XfCC6TVVb3ZzjgnRlSFrLwhtGbK5mHeotYzYkmlv5tsnV8bcBH/Io2IbYcUdFS+Z+nHAfTRFkzO2hcViy4V2Ss1IK5WRDDGVeimNCTEwa6UTai8zcOPHgutz0GqVQNTVtiiT1RWEKvS8gchssdXBJQx7sFEVJ5V5A/IaDXnzIAmcD2K4ttrCNWlE49hho/PfY8SeETvzswkowiKzyUleKsnXuXZQl2uQzvU1OEVH9u8Q7YxIEXCoDSoA75xQWbyGgNa9vKPZQ=";
 		
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 		X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(
@@ -31,7 +31,7 @@ public class LicenseReader {
 	    
 	    byte[] decryptedBytes = decrpyptCipher.doFinal(Base64.getDecoder().decode(licenseKey));
 	    String license = new String(decryptedBytes);
-	    String[] split = license.split(":");
+	    String[] split = license.split("_:_");
 	    System.out.println("Licensed to " + split[0] + ", valid till " + new Date(Long.valueOf(split[1])) + ", type " + split[2]);
 
 	    
