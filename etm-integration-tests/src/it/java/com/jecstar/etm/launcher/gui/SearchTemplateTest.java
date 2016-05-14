1455805d377ce0b7048c9c16c0ac3b5d01192522
Mark Holster <m.holster@home.nl>
2016-05-14 13:38:33 +0200

    First setup of integration test.

--- etm-integration-tests/src/it/java/com/jecstar/etm/launcher/gui/SearchTemplateTest.java
+++ etm-integration-tests/src/it/java/com/jecstar/etm/launcher/gui/SearchTemplateTest.java
@@ -1,5 +1,8 @@
 package com.jecstar.etm.launcher.gui;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,18 +35,41 @@ public class SearchTemplateTest {
 	}
 	
 	@Test
-	public void testTemplateManagerment() {
+	public void testTemplateManagement() {
 	    this.driver.get("http://127.0.0.1:8080/gui/search/index.html");             
 	    this.driver.findElement(By.id("j_username")).sendKeys("admin");     
 	    this.driver.findElement(By.id("j_password")).sendKeys("password");     
 	    this.driver.findElement(By.className("btn")).submit();
 	    
+	    
+	    // Search button should be disabled when no query entered yet
+	    assertFalse("Search button is enabled when search string is not provided", this.driver.findElement(By.id("btn-search")).isEnabled());
+	    assertFalse("Template name input is enabled when search string is not provided", this.driver.findElement(By.id("template-name")).isEnabled());
+	    assertFalse("Template save button is enabled when search string is not provided", this.driver.findElement(By.id("btn-save-template")).isEnabled());
 	    this.driver.findElement(By.id("query-string")).sendKeys("*");
+	    assertTrue("Search button is not enabled when search string is provided", this.driver.findElement(By.id("btn-search")).isEnabled());
+	    assertTrue("Template name input is not enabled when search string is provided", this.driver.findElement(By.id("template-name")).isEnabled());
+	    assertFalse("Template save button is enabled when template name is not provided", this.driver.findElement(By.id("btn-save-template")).isEnabled());
+	    
 	    this.driver.findElement(By.id("btn-search")).click();
-	    (new WebDriverWait(this.driver, 10)).until(new ExpectedCondition<Boolean>() {
+	    new WebDriverWait(this.driver, 10).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
             	try {
             		d.findElement(By.id("search_result_table"));
+            		return true;
+            	} catch (NoSuchElementException e) {}
+            	return false;
+            }
+        });
+	    
+	    
+	    this.driver.findElement(By.id("template-name")).sendKeys("Integration-test");
+	    assertTrue("Template save button is enabled when template name is not provided", this.driver.findElement(By.id("btn-save-template")).isEnabled());
+	    this.driver.findElement(By.id("btn-save-template")).click();
+	    new WebDriverWait(this.driver, 10).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+            	try {
+            		d.findElement(By.id("list-template-links")).findElement(By.xpath("./li/a[text()='Integration-test']"));
             		return true;
             	} catch (NoSuchElementException e) {}
             	return false;
