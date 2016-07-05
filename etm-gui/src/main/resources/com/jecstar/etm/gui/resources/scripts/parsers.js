c11b7362b0aebaa74d205a38803a7d6317fbbd6f
Mark Holster <m.holster@jecstar.com>
2016-07-05 22:03:20 +0200

    Client side of parsers finished.

--- etm-gui/src/main/resources/com/jecstar/etm/gui/resources/scripts/parsers.js
+++ etm-gui/src/main/resources/com/jecstar/etm/gui/resources/scripts/parsers.js
@@ -1,18 +1,18 @@
 function buildParserPage() {
 	var parserMap = {};
 	$('#input-parser-type').change(function() {
-		$('#grp-fixed_position,#grp-fixed_value,#grp-jsonpath,#grp-xpath,#grp-xslt').hide();
+		$('#grp-fixed_position, #grp-fixed_value, #grp-jsonpath, #grp-xpath,#grp-xslt').hide();
+		// Disable all input fields otherwise form validation will fail.
+		$('#grp-fixed_position > input, #grp-fixed_value > input, #grp-jsonpath > input, #grp-xpath > input ,#grp-xslt > textarea').attr('disabled', 'disabled');
+		$('#grp-' +$(this).val() + ' > input').removeAttr('disabled'); 
+		$('#grp-' +$(this).val() + ' > textarea').removeAttr('disabled');
 		$('#grp-' +$(this).val()).show(); 
 	});
 
 	$('#sel-parser').change(function() {
 		var parserData = parserMap[$(this).val()];
-		if ("undefined" == typeof parserData) {
-			$('#input-parser-name').val('');
-			$('#input-parser-type').val('fixed_position').change();
-			$('#input-fixed_position-line').val(1)
-			$('#input-fixed_position-start-ix').val(1);
-			$('#input-fixed_position-end-ix').val(2)
+		if ('undefined' == typeof parserData) {
+			resetValues();
 			return;
 		}
 		
@@ -31,7 +31,39 @@ function buildParserPage() {
 		} else if ('xslt' == parserData.type) {
 			$('#input-xslt-template').val(parserData.template);
 		}
+		enableOrDisableButtons();
+	});
+	
+	$('#btn-confirm-save-parser').click(function(event) {
+		event.preventDefault();
+		if (!document.getElementById('parser_form').checkValidity()) {
+			return;
+		}
+		var parserName = $('#input-parser-name').val();
+		if (isParserExistent(parserName)) {
+			$('#overwrite-parser-name').text(parserName);
+			$('#modal-parser-overwrite').modal();
+		} else {
+			saveParser();
+		}
+	});
+	
+	$('#btn-save-parser').click(function(event) {
+		saveParser();
+	});
+	
+	$('#btn-confirm-remove-parser').click(function(event) {
+		event.preventDefault();
+		$('#remove-parser-name').text($('#input-parser-name').val());
+        $('#modal-parser-remove').modal();
 	});	
+
+	$('#btn-remove-parser').click(function(event) {
+		removeParser($('#input-parser-name').val());
+	});
+
+	
+	$('#input-parser-name').on('input', enableOrDisableButtons);
 	
 	$.ajax({
 	    type: 'GET',
@@ -42,15 +74,89 @@ function buildParserPage() {
 	            return;
 	        }
 	        $parserSelect = $('#sel-parser');
-	        $.each(data.parsers.sort(sortParsersByName), function(index, parser) {
+	        $.each(data.parsers, function(index, parser) {
 	        	$parserSelect.append($('<option>').attr('value', parser.name).text(parser.name));
 	        	parserMap[parser.name] = parser;
 	        });
+	        sortSelectOptions($parserSelect)
+	        $parserSelect.val('');
 	    }
 	});
 	
-	function sortParsersByName(a, b){
-	  return ((a.name < b.name) ? -1 : ((a.name > b.name) ? 1 : 0));
+	function sortSelectOptions($parserSelect) {
+		var options = $parserSelect.children('option');
+		options.detach().sort(function(a,b) {
+		    var at = $(a).text();
+		    var bt = $(b).text();         
+		    return (at > bt) ? 1 : ((at < bt) ? -1 : 0);
+		});
+		options.appendTo($parserSelect);
+	}
+	
+	function enableOrDisableButtons() {
+		var parserName = $('#input-parser-name').val();
+		if (parserName) {
+			$('#btn-confirm-save-parser').removeAttr('disabled');
+			if (isParserExistent(parserName)) {
+				$('#btn-confirm-remove-parser').removeAttr('disabled');
+			} else {
+				$('#btn-confirm-remove-parser').attr('disabled', 'disabled');
+			}
+		} else {
+			$('#btn-confirm-save-parser, #btn-confirm-remove-parser').attr('disabled', 'disabled');
+		}
+	}
+	
+	function isParserExistent(name) {
+		return "undefined" != typeof parserMap[name];
+	}
+	
+	function saveParser() {
+		var parserData = createParserData();
+		if (!isParserExistent(parserData.name)) {
+			$parserSelect = $('#sel-parser');
+			$parserSelect.append($('<option>').attr('value', parserData.name).text(parserData.name));
+			sortSelectOptions($parserSelect);
+		}
+		parserMap[parserData.name] = parserData;
+		$('#parsers_infoBox').text('Parser \'' + parserData.name + '\' saved.').show('fast').delay(5000).hide('fast');
+		$('#modal-parser-overwrite').modal('hide');
+	}
+	
+	function removeParser(parserName) {
+		delete parserMap[parserName];
+		$('#sel-parser > option[value=\'' + parserName + '\']').remove();
+		$('#modal-parser-remove').modal('hide');
+		$('#parsers_infoBox').text('Parser \'' + parserName + '\' removed.').show('fast').delay(5000).hide('fast');
+	}
+	
+	function createParserData() {
+		var parserData = {
+			name: $('#input-parser-name').val(),
+			type: $('#input-parser-type').val()
+		}
+		if ('fixed_position' == parserData.type) {
+			parserData['line'] = $('#input-fixed_position-line').val() - 1;
+			parserData['start_ix'] = $('#input-fixed_position-start-ix').val() - 1;
+			parserData['end_ix'] = $('#input-fixed_position-end-ix').val() - 1;
+		} else if ('fixed_value' == parserData.type) {
+			parserData['value'] = $('#input-fixed-value').val();
+		} else if ('jsonpath' == parserData.type) {
+			parserData['expression'] = $('#input-jsonpath-expression').val();
+		} else if ('xpath' == parserData.type) {
+			parserData['expression'] = $('#input-xpath-expression').val();
+		} else if ('xslt' == parserData.type) {
+			parserData['template'] = $('#input-xslt-template').val();
+		}
+		return parserData;
 	}
 
+	function resetValues() {
+		$('#input-parser-name').val('');
+		$('#input-parser-type').val('fixed_position').change();
+		$('#input-fixed_position-line').val(1)
+		$('#input-fixed_position-start-ix').val(1);
+		$('#input-fixed_position-end-ix').val(2)
+		enableOrDisableButtons();
+	}
 }
