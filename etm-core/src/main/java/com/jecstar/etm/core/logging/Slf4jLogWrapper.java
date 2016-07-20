c7e32d34b6853a3a1a5bf0c6ccd980d34e972780
Mark Holster <m.holster@home.nl>
2016-07-20 15:07:40 +0200

    Log correct linenumber

--- etm-core/src/main/java/com/jecstar/etm/core/logging/Slf4jLogWrapper.java
+++ etm-core/src/main/java/com/jecstar/etm/core/logging/Slf4jLogWrapper.java
@@ -2,6 +2,7 @@ package com.jecstar.etm.core.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 public class Slf4jLogWrapper extends AbstractDelegateLogWrapper {
 
@@ -9,6 +10,11 @@ public class Slf4jLogWrapper extends AbstractDelegateLogWrapper {
      * The slf4j logger.
      */
     private final Logger logger;
+    
+    /**
+     * The logger class name.
+     */
+    private final String FQCN = AbstractDelegateLogWrapper.class.getName();
 
     /**
      * Constructs a new <code>Slf4jLogWrapper</code> instance.
@@ -73,27 +79,47 @@ public class Slf4jLogWrapper extends AbstractDelegateLogWrapper {
 
 	@Override
     protected void logDelegatedDebugMessage(String message, Throwable throwable) {
-		this.logger.debug(message, throwable);
+		if (this.logger instanceof LocationAwareLogger) {
+			 ((LocationAwareLogger)this.logger).log(null, FQCN, LocationAwareLogger.DEBUG_INT, message, null, throwable);
+		} else {
+			this.logger.debug(message, throwable);
+		}
     }
 
 	@Override
     protected void logDelegatedInfoMessage(String message, Throwable throwable) {
-	    this.logger.info(message, throwable);
+		if (this.logger instanceof LocationAwareLogger) {
+			 ((LocationAwareLogger)this.logger).log(null, FQCN, LocationAwareLogger.INFO_INT, message, null, throwable);
+		} else {
+			this.logger.info(message, throwable);
+		}
     }
 
 	@Override
     protected void logDelegatedWarningMessage(String message, Throwable throwable) {
-	    this.logger.warn(message, throwable);
-    }
+		if (this.logger instanceof LocationAwareLogger) {
+			 ((LocationAwareLogger)this.logger).log(null, FQCN, LocationAwareLogger.WARN_INT, message, null, throwable);
+		} else {
+			this.logger.warn(message, throwable);
+		}
+	}
 
 	@Override
     protected void logDelegatedErrorMessage(String message, Throwable throwable) {
-	    this.logger.error(message, throwable);
-    }
+		if (this.logger instanceof LocationAwareLogger) {
+			 ((LocationAwareLogger)this.logger).log(null, FQCN, LocationAwareLogger.ERROR_INT, message, null, throwable);
+		} else {
+			this.logger.error(message, throwable);
+		}
+	}
 
 	@Override
     protected void logDelegatedFatalMessage(String message, Throwable throwable) {
-	    this.logger.error(message, throwable);
-    }
+		if (this.logger instanceof LocationAwareLogger) {
+			 ((LocationAwareLogger)this.logger).log(null, FQCN, LocationAwareLogger.ERROR_INT, message, null, throwable);
+		} else {
+			this.logger.error(message, throwable);
+		}
+	}
 	
 }
