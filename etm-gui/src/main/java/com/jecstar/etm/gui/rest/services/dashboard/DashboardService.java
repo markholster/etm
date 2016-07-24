0a9957cce92c062487fba4afe801a191604b2c27
Mark Holster <m.holster@jecstar.com>
2016-07-24 10:03:24 +0200

    Try to create a dynamic line chart.

--- etm-gui/src/main/java/com/jecstar/etm/gui/rest/services/dashboard/DashboardService.java
+++ etm-gui/src/main/java/com/jecstar/etm/gui/rest/services/dashboard/DashboardService.java
@@ -1,8 +1,22 @@
 package com.jecstar.etm.gui.rest.services.dashboard;
 
-import javax.ws.rs.Path;
+import java.util.Map;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
+import org.joda.time.DateTimeZone;
 
 import com.jecstar.etm.gui.rest.AbstractJsonService;
 import com.jecstar.etm.server.core.configuration.EtmConfiguration;
@@ -16,5 +30,79 @@ public class DashboardService extends AbstractJsonService {
 	public static void initialize(Client client, EtmConfiguration etmConfiguration) {
 		DashboardService.client = client;
 		DashboardService.etmConfiguration = etmConfiguration;
+	}
+	
+	
+	@POST
+	@Path("/chart")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getChartData(String json) {
+		Map<String, Object> valueMap = toMap(json);
+		String type = getString("type", valueMap);
+		if ("line".equals(type)) {
+			return getDataForLineChart(valueMap);
+		}
+		return null;
+	}
+
+
+	private String getDataForLineChart(Map<String, Object> valueMap) {
+		StringBuilder result = new StringBuilder();
+		result.append("[");
+		SearchRequestBuilder searchRequestBuilder = client.prepareSearch(getString("index", valueMap))
+			.setSize(0)
+			.setFetchSource(false)
+			.setTimeout(TimeValue.timeValueMillis(etmConfiguration.getQueryTimeout()));
+		Map<String, Object> aggregationValues = getObject("agg", valueMap);
+		AggregationBuilder aggregationBuilder = createAggregations(aggregationValues);
+		if (aggregationBuilder != null) {
+			searchRequestBuilder.addAggregation(aggregationBuilder);
+		}
+		SearchResponse searchResponse = searchRequestBuilder.get();
+		Map<String, Object> xAxisValues = getObject("x_axis", aggregationValues);
+		Map<String, Object> yAxisValues = getObject("y_axis", aggregationValues);
+		String[] xSelectors = getString("selector", xAxisValues).split("->");
+		String[] ySelectors = getString("selector", xAxisValues).split("->");
+		
+		Aggregations aggregations = searchResponse.getAggregations();
+		Aggregation aggregation = aggregations.get("Events over time");
+		
+		
+		result.append("]");
+		return result.toString();
+	}
+
+
+	private AggregationBuilder createAggregations(Map<String, Object> aggregationValues) {
+		String type = getString("type", aggregationValues);
+		if ("date-histogram".equals(type)) {
+			return AggregationBuilders.dateHistogram(getString("name",aggregationValues))
+				.timeZone(DateTimeZone.forID(getEtmPrincipal().getTimeZone().getID()))
+				.field(getString("field", aggregationValues))
+				.dateHistogramInterval(getDateHistogramInterval(getString("interval", aggregationValues)));
+		}
+		return null;
+	}
+
+
+	private DateHistogramInterval getDateHistogramInterval(String interval) {
+		if ("day".equals(interval)) {
+			return DateHistogramInterval.DAY;
+		} else if ("hour".equals(interval)) {
+			return DateHistogramInterval.HOUR;
+		} else if ("minute".equals(interval)) {
+			return DateHistogramInterval.MINUTE;
+		} else if ("month".equals(interval)) {
+			return DateHistogramInterval.MONTH;
+		} else if ("quarter".equals(interval)) {
+			return DateHistogramInterval.QUARTER;
+		} else if ("second".equals(interval)) {
+			return DateHistogramInterval.SECOND;
+		} else if ("week".equals(interval)) {
+			return DateHistogramInterval.WEEK;
+		} else if ("year".equals(interval)) {
+			return DateHistogramInterval.YEAR;
+		}
+		return null;
 	}
 }
--- etm-gui/src/main/resources/com/jecstar/etm/gui/resources/scripts/dashboard.js
+++ etm-gui/src/main/resources/com/jecstar/etm/gui/resources/scripts/dashboard.js
@@ -4,16 +4,30 @@ function loadDashboard(name) {
 			rows: [
 			  { height: 16,
 				cols: [
-				  { parts: 6,
+				  { 
+					id: '3523523',
+					parts: 6,
 					title: 'Test colom 1',
 					bordered: true,
-					type: 'bar'
-				  },
-				  { parts: 6,
-					title: 'Test colom 2',
-					bordered: true,
-					type: 'pie'
-				  }				  
+					data: {
+						index: 'etm_event_all',
+						type: 'line',
+						agg: {
+							name: 'Events over time',
+							type: 'date-histogram',
+							field: 'endpoints.writing_endpoint_handler.handling_time',
+							interval: 'hour'
+						},
+						x_axis: { 
+							selector: 'Events over time->$key',
+							label: 'Events over time'
+						},
+						y_axis: {
+							selector: 'Events over time->$doc_count',
+							label: 'Count'
+						}
+					}
+				  }			  
 				]
 			  }
 			]	
@@ -43,19 +57,81 @@ function loadDashboard(name) {
 			$.each(row.cols, function (colIx, col) {
 				var colContainer = $('<div>').addClass('col-md-' + col.parts).attr('style', 'height: 100%;');
 				var svgContainer = colContainer;
+				var svg = $('<svg>').attr('id', col.id).attr('style', 'height: 100%; display: block;');
 				if (col.bordered) {
 					var card = $('<div>').addClass('card card-block').attr('style', 'height: 100%;');
 					svgContainer = card;
 					colContainer.append(card);
+					svg.addClass('card-img');
 				}
 				svgContainer.append(
 				  $('<h5>').addClass('card-title').text(col.title).append(
 				    $('<i>').addClass('fa fa-pencil-square-o pull-right')
-				  )
+				  ),
+				  svg
 				);
 				rowContainer.append(colContainer);
+				// Now load the data.
+			    $.ajax({
+			        type: 'POST',
+			        contentType: 'application/json',
+			        url: '../rest/dashboard/chart',
+			        data: JSON.stringify(col.data),
+			        success: function(data) {
+			            if (!data) {
+			                return;
+			            }
+			            if ('line' == col.data.type) {
+			            	renderLineChart(svg, col.data, data);
+			            }
+			        }
+			    });
 			});
 			graphContainer.append(rowContainer);
 		});
 	}
+	
+	function renderLineChart(svg, config, data) {
+        nv.addGraph(function() {
+      	  var chart = nv.models.lineChart()
+      	                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+      	                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+      	                .transitionDuration(350)  //how fast do you want the lines to transition?
+      	                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+      	                .showYAxis(true)        //Show the y-axis
+      	                .showXAxis(true)        //Show the x-axis
+      	  ;
+
+      	  chart.xAxis     //Chart x-axis settings
+      	      .axisLabel(config.x_axis.label)
+      	      .tickFormat(d3.format(',r'));
+
+      	  chart.yAxis     //Chart y-axis settings
+      	      .axisLabel(config.y_axis.label)
+      	      .tickFormat(d3.format('.02f'));
+
+      	  d3.select(svg.get(0))    //Select the <svg> element you want to render the chart in.   
+      	      .datum(data)         //Populate the <svg> element with chart data...
+      	      .call(chart);          //Finally, render the chart!
+
+      	  //Update the chart when window resizes.
+      	  nv.utils.windowResize(function() { chart.update() });
+      	  return chart;
+      });			            
+	}
+	
+	// TODO deze functie moet gebruikt worden tijdens het opslaan van een grafiek. Het ID moet dan geset worden naar een unieke waarde
+	function generateUUID() {
+    	var d = new Date().getTime();
+	    if(window.performance && typeof window.performance.now === "function"){
+	        d += performance.now(); //use high-precision timer if available
+	    }
+	    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	        var r = (d + Math.random()*16)%16 | 0;
+	        d = Math.floor(d/16);
+	        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	    });
+	    return uuid;
+	}
+
 }
