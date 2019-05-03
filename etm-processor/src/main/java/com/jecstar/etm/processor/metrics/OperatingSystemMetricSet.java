1383b3fac1b19f93996c3bb777b3e5a29ad1706c
Mark Holster <m.holster@jecstar.com>
2019-05-03 16:53:10 +0200

    Better os monitoring.

--- etm-processor/src/main/java/com/jecstar/etm/processor/metrics/OperatingSystemMetricSet.java
+++ etm-processor/src/main/java/com/jecstar/etm/processor/metrics/OperatingSystemMetricSet.java
@@ -6,23 +6,103 @@ import com.codahale.metrics.MetricSet;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.util.Collections;
-import java.util.HashMap;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 public class OperatingSystemMetricSet implements MetricSet {
+
+    private final Method getFreePhysicalMemorySize;
+    private final Method getTotalPhysicalMemorySize;
+    private final Method getFreeSwapSpaceSize;
+    private final Method getTotalSwapSpaceSize;
+    private final Method getSystemLoadAverage;
+    private final Method getSystemCpuLoad;
 
     private final OperatingSystemMXBean os;
 
     public OperatingSystemMetricSet() {
         this.os = ManagementFactory.getOperatingSystemMXBean();
+        this.getFreePhysicalMemorySize = getMethod("getFreePhysicalMemorySize");
+        this.getTotalPhysicalMemorySize = getMethod("getTotalPhysicalMemorySize");
+        this.getFreeSwapSpaceSize = getMethod("getFreeSwapSpaceSize");
+        this.getTotalSwapSpaceSize = getMethod("getTotalSwapSpaceSize");
+        this.getSystemLoadAverage = getMethod("getSystemLoadAverage");
+        this.getSystemCpuLoad = getMethod("getSystemCpuLoad");
+    }
+
+    private Method getMethod(String methodName) {
+        try {
+            return Class.forName("com.sun.management.OperatingSystemMXBean").getMethod(methodName);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public Map<String, Metric> getMetrics() {
-        final Map<String, Metric> gauges = new HashMap<>();
-        gauges.put("os.cpu.system_load_average", (Gauge<Double>) os::getSystemLoadAverage);
-        return Collections.unmodifiableMap(gauges);
+        return Map.of(
+                "os.cpu.system_load_average", (Gauge<Double>) os::getSystemLoadAverage,
+                "os.cpu.system_load_percentage", (Gauge<Double>) OperatingSystemMetricSet.this::getLoadPercentage,
+                "os.mem.total", (Gauge<Long>) OperatingSystemMetricSet.this::getTotalPhysicalMemorySize,
+                "os.mem.free", (Gauge<Long>) OperatingSystemMetricSet.this::getFreePhysicalMemorySize,
+                "os.swap.total", (Gauge<Long>) OperatingSystemMetricSet.this::getTotalSwapSpaceSize,
+                "os.swap.free", (Gauge<Long>) OperatingSystemMetricSet.this::getFreeSwapSpaceSize
+        );
+    }
+
+    private double getLoadPercentage() {
+        if (this.getSystemCpuLoad != null) {
+            try {
+                return (double) this.getSystemCpuLoad.invoke(this.os);
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    private long getFreePhysicalMemorySize() {
+        if (this.getFreePhysicalMemorySize == null) {
+            return -1;
+        }
+        try {
+            return (long) this.getFreePhysicalMemorySize.invoke(this.os);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private long getTotalPhysicalMemorySize() {
+        if (this.getTotalPhysicalMemorySize == null) {
+            return -1;
+        }
+        try {
+            return (long) this.getTotalPhysicalMemorySize.invoke(this.os);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private long getFreeSwapSpaceSize() {
+        if (this.getFreeSwapSpaceSize == null) {
+            return -1;
+        }
+        try {
+            return (long) this.getFreeSwapSpaceSize.invoke(this.os);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private long getTotalSwapSpaceSize() {
+        if (this.getTotalSwapSpaceSize == null) {
+            return -1;
+        }
+        try {
+            return (long) this.getTotalSwapSpaceSize.invoke(this.os);
+        } catch (Exception e) {
+            return -1;
+        }
     }
 
 }
